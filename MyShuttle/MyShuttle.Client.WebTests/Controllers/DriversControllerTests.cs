e653a2917671ef699414e3e7cb8366ad767bd7ee
kendrahavens <kehavens@microsoft.com>
2017-11-14 17:35:31 -0800

    Add test stubs

---
+++ MyShuttle/MyShuttle.Client.WebTests/Controllers/DriversControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShuttle.Client.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShuttle.Client.Web.Controllers.Tests
+{
+    [TestClass()]
+    public class DriversControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
